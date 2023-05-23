66db0942923bad1bf4f8b5f0ee4eac0620ae4210
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-05-23 07:47:03 +0100

    first commit

---
+++ src/Search.jsx
@@ -0,0 +1,43 @@
+import { useEffect,useState } from "react";
+import "./Search.css";
+
+function Search() {
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data)
+    
+        setFilterData(data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const [data, setData] = useState([])
+  const [filterData, setFilterData] = useState([])
+
+  const handleFilter = (value) => {
+    const res = filterData.filter(f => f.name.toLowerCase().includes(value))
+    setData(res)
+    if (value === "" ){
+        setData([])
+    }
+
+  }
+  return (
+    <div className="search-top">
+      <div className="search">
+        <input type="text" placeholder="search here" onChange={e =>handleFilter(e.target.value)} />
+      </div>
+      <div className="search-result">
+      {data.map((d,i) =>(
+        <div key={i}>
+            {d.name}
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default Search;
